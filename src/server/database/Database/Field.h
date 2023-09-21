freadblangks/Aquayoup-SL

Initial Eluna Files
Its broken and doesn't compile correctly. Please do not use.

src/server/database/Database/Field.h
@@ -121,6 +121,7 @@ class TC_DATABASE_API Field
             return data.value == nullptr;
         }
 
+        DatabaseFieldTypes GetType() { return meta->Type; }
     protected:
         struct
         {

Valeomega/MewCore

src/server/database/Database/Field.h
@@ -127,6 +127,8 @@ class TC_DATABASE_API Field
             return _value == nullptr;
         }
 
+        DatabaseFieldTypes GetType() { return _meta->Type; }
+   
     private:
         char const* _value;             // Actual data in memory
         uint32 _length;                 // Length